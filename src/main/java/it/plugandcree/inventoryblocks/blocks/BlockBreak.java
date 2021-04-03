21e47cf5fd47132b17710699c9b6925f306b5d0e
PlugAndCree <nicolamarangon123@gmail.com>
2021-04-03 22:22:25 +0200

    Added blockBreak logic

--- src/main/java/it/plugandcree/inventoryblocks/blocks/BlockBreak.java
+++ src/main/java/it/plugandcree/inventoryblocks/blocks/BlockBreak.java
@@ -1,14 +1,22 @@
 package it.plugandcree.inventoryblocks.blocks;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class BlockBreak implements Listener {
-	
+
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
-		
+		if (!e.getPlayer().hasPermission("inventoryblocks.use"))
+			return;
+
+		Player p = e.getPlayer();
+
+		e.setDropItems(false);
+
+		e.getBlock().getDrops(p.getInventory().getItemInMainHand()).forEach(p.getInventory()::addItem);
 	}
-	
+
 }
