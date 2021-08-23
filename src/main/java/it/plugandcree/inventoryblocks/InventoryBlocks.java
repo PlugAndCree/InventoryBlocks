c0be6788a36d9c74f17ae286dcbd74c1b2989062
PlugAndCree <nicolamarangon123@gmail.com>
2021-08-23 15:15:32 +0200

    Added full inventory check

--- src/main/java/it/plugandcree/inventoryblocks/InventoryBlocks.java
+++ src/main/java/it/plugandcree/inventoryblocks/InventoryBlocks.java
@@ -6,9 +6,18 @@ import it.plugandcree.inventoryblocks.blocks.BlockBreak;
 
 public class InventoryBlocks extends JavaPlugin {
 	
+	private static InventoryBlocks instance;
+	
 	@Override
 	public void onEnable() {
+		instance = this;
+		
 		getServer().getPluginManager().registerEvents(new BlockBreak(), this);
 	}
+
+	public static InventoryBlocks getInstance() {
+		return instance;
+	}
+	
 	
 }
--- src/main/java/it/plugandcree/inventoryblocks/blocks/BlockBreak.java
+++ src/main/java/it/plugandcree/inventoryblocks/blocks/BlockBreak.java
@@ -1,24 +1,43 @@
 package it.plugandcree.inventoryblocks.blocks;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import it.plugandcree.inventoryblocks.InventoryBlocks;
+import net.md_5.bungee.api.ChatColor;
+
 public class BlockBreak implements Listener {
 
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
-		if (!e.getPlayer().hasPermission("inventoryblocks.use"))
-			return;
-		
-		if (e.isCancelled()) return;
-		
 		Player p = e.getPlayer();
+
+		if (!p.hasPermission("inventoryblocks.use"))
+			return;
+
+		if (e.isCancelled())
+			return;
+
+		if (!(p.getGameMode() == GameMode.SURVIVAL))
+			return;
 
 		e.setDropItems(false);
 
 		e.getBlock().getDrops(p.getInventory().getItemInMainHand()).forEach(p.getInventory()::addItem);
+
+		if (p.getInventory().firstEmpty() == -1) {
+			p.sendTitle(
+					ChatColor.translateAlternateColorCodes('&',
+							InventoryBlocks.getInstance().getConfig().getString("inventory-full.title")),
+					ChatColor.translateAlternateColorCodes('&',
+							InventoryBlocks.getInstance().getConfig().getString("inventory-full.subtitle")),
+					InventoryBlocks.getInstance().getConfig().getInt("inventory-full.fadein"),
+					InventoryBlocks.getInstance().getConfig().getInt("inventory-full.stay"),
+					InventoryBlocks.getInstance().getConfig().getInt("inventory-full.fadeout"));
+		}
 	}
 
 }
